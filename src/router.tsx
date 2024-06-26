a57306f8f5ab0dfcc82e43d5457037324639a9a8
Justin Altamirano <jam.altamirano@yavirac.edu.ec>
2024-06-26 15:06:16 -0500

    se instalo  el gh-pages

--- src/router.tsx
+++ src/router.tsx
@@ -4,7 +4,7 @@ import ProfileJustin from './pages/profile-justin-altamirano';
 
 function AppRouter () {
   return (
-    <Router>
+    <Router basename='/Justinarley.io'>
       <Routes>
         <Route path="/" element={<Profiles />} />
         <Route path="/justin" element={<ProfileJustin />} />
