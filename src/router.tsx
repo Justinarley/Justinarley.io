21831567fbc1b7731ec73d0e583e21a7b2856f7d
Justin Altamirano <jam.altamirano@yavirac.edu.ec>
2024-06-26 15:18:43 -0500

    Pruebas 2

--- src/router.tsx
+++ src/router.tsx
@@ -4,7 +4,7 @@ import ProfileJustin from './pages/profile-justin-altamirano';
 
 function AppRouter () {
   return (
-    <Router basename='/Justinarley.io'>
+    <Router basename='/Portafolio'>
       <Routes>
         <Route path="/" element={<Profiles />} />
         <Route path="/justin" element={<ProfileJustin />} />
